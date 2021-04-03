0c71d0c469f92f2f131dcff664a678fd5389a9a2
gabbiebolcer <gkbolcer@stthomas.edu>
2021-04-03 14:00:48 -0500

    make a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='RentPaymentMessage',
+    version='0.0.1',
+    author='Gabbie Bolcer',
+    author_email='gkbolcer@stthomas.edu',
+    description='Package that sends your roomates a reminder to pay their rent!',
+    url='https://github.com/gabbiebolcer/RentPaymentMessage',
+    python_requires=">=3.8"
+)
